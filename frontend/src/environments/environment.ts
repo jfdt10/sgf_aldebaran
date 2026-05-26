967c2a43afe3a99f5ccfe817725e0ca46e31ed4d
jfdt10 <jeanlipe1010@gmail.com>
2026-05-25 23:44:26 -0300

    chore: atualizar apiUrl para producao

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Para produção, substitua a URL abaixo pela URL do seu backend no Render (ex: 'https://seu-backend.onrender.com')
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://aldebaran-backend-jusq.onrender.com'
 };
